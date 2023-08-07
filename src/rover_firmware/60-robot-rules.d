9f0b442998fb084c20967aaaf8a71f2b2729a839
KshitijBhat <kmbhat@lakeheadu.ca>
2023-08-07 20:23:23 +0000

    udev rules

---
+++ src/rover_firmware/60-robot-rules.d
@@ -0,0 +1,7 @@
+KERNEL=="ttyACM*", ATTRS{idVendor}=="2e8a", ATTRS{idProduct}=="000a", ATTRS{serial}=="E660B440073CAA26", MODE:="0666", SYMLINK+="fl_pico"
+
+KERNEL=="ttyACM*", ATTRS{idVendor}=="2e8a", ATTRS{idProduct}=="000a", ATTRS{serial}=="E660C0D1C73E9930", MODE:="0666", SYMLINK+="fr_pico"
+
+KERNEL=="ttyACM*", ATTRS{idVendor}=="2e8a", ATTRS{idProduct}=="000a", ATTRS{serial}=="E660B4400755922C", MODE:="0666", SYMLINK+="rl_pico"
+
+KERNEL=="ttyACM*", ATTRS{idVendor}=="2e8a", ATTRS{idProduct}=="000a", ATTRS{serial}=="E660C0D1C716832B", MODE:="0666", SYMLINK+="rr_pico"
